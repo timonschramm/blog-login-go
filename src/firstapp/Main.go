b5216c6ec4e9ff60211322959465bbc5b2954d28
timonschramm <timonschramm.prod@gmail.com>
2021-11-01 10:15:31 +0100

    worked with templating

--- src/firstapp/Main.go
+++ src/firstapp/Main.go
@@ -1,35 +1,31 @@
 package main
 
+//https://stackoverflow.com/questions/37328834/how-to-specify-the-file-location-for-template-parsefiles-in-go-language !!
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
-func index_Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hey there</h1>")
-	fmt.Fprintf(w, "<p> this is HTML</p>")
-	fmt.Fprintf(w, "You can %s add %s", "even", "something cool")
-
-	fmt.Fprintf(w, `<h1>Hey there</h1>
-					<p> this is HTML</p>
-	`)
+type NewsAggPage struct {
+	Title string
+	News  string
 }
+
+func newsAggHandler(w http.ResponseWriter, r *http.Request) {
+	p := NewsAggPage{Title: "Amazing News Aggregator", News: "some news"}
+	fmt.Println(p)
+	t := template.Must(template.ParseFiles("basictemplating.html"))
+	fmt.Println(t)
+	t.Execute(w, p)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1> Hello World </h1>")
+}
+
 func main() {
-	http.HandleFunc("/", index_Handler)
-	http.ListenAndServe(":8000", nil)
-}
-
-func main2() {
-	fmt.Println("Enter your name:")
-	var first string
-	fmt.Scanln(&first)
-	fmt.Println("Dein Name ist also " + first)
-
-	fmt.Println("Bitte gib dein Username ein:")
-	var zUsername string
-	fmt.Scanln(&zUsername)
-	fmt.Println("Bitte gib dein Passwort ein.")
-	var zPassword string
-	fmt.Scanln(&zPassword)
-	fmt.Println("Hallo " + zUsername + " dein Passwort ist: " + zPassword)
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/agg/", newsAggHandler)
+	http.ListenAndServe(":80", nil)
 }
